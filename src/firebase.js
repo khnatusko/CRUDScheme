60ffe3f6384804f8f67da200763daec961102cb9
Krzysztof <hnatusko.k@gmail.com>
2021-10-27 18:56:47 +0200

    First

---
+++ src/firebase.js
@@ -0,0 +1,32 @@
+import firebase from 'firebase/compat/app'
+
+import 'firebase/compat/auth'
+
+import 'firebase/compat/firestore'
+
+import 'firebase/compat/storage'
+
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCm2e6O0AwHBuUzFxeJfmcT9VAeerlao9w",
+    authDomain: "pwig-8036b.firebaseapp.com",
+    projectId: "pwig-8036b",
+    storageBucket: "pwig-8036b.appspot.com",
+    messagingSenderId: "655079150628",
+    appId: "1:655079150628:web:cd224020309285d43c74b1"
+  }
+
+  firebase.initializeApp(firebaseConfig)
+
+  const db = firebase.firestore()
+  const auth = firebase.auth()
+  const storage = firebase.storage()
+
+  const usersCollection = db.collection('users')
+
+  export {
+      db,
+      auth,
+      storage,
+  }
--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,25 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/create',
+    name: 'Add',
+    component: () => import('../components/UserCreate')
+  },
+  {
+    path: '/list',
+    name: 'List',
+    component: () => import('../components/UserList')
+  },
+  {
+    path: '/edit',
+    name: 'Edit',
+    component: () => import('../components/UserEdit')
   }
+
 ]
 
 const router = new VueRouter({
